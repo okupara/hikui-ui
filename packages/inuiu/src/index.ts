c528e9c134e4b363fc4de96d9f481219812ff3c0
okupara <okuharakensuke@gmail.com>
2023-10-18 22:01:57 +0900

    Setup to build as a library with vite

---
+++ packages/inuiu/src/index.ts
@@ -0,0 +1,3 @@
+export * from "@/components/ui/button";
+export * from "@/components/ui/input";
+export * from "@/lib/hikui-theme-preset";
--- packages/inuiu/vite.config.ts
+++ packages/inuiu/vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: [
+        "src/index.ts",
+        "src/components",
+        "src/lib/hikui-theme-preset.ts",
+      ],
+      exclude: ["src/**/*.stories.tsx"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react/jsx-runtime",
+        "tailwindcss",
+        "tailwindcss/plugin",
+        "tailwindcss-animate",
+      ],
+    },
+    copyPublicDir: false,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
